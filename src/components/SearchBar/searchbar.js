fe2516e5b5c797c43c556fa502de79ce7cca00d7
jefferyclanton <37197975+jefferyclanton@users.noreply.github.com>
2018-04-15 14:13:22 -0400

    Fix search bar code error

--- src/components/SearchBar/searchbar.js
+++ src/components/SearchBar/searchbar.js
@@ -8,30 +8,23 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component {
-  renderSortByOptions() {
-    return SearchBar.keys(sortByOptions).map(sortByOptions => {
-      let sortByOptionsValue = sortByOptions[sortByOptions];
-      return <li key={sortByOptionsValue}>{sortByOptions}</li>;
-    });
-  }
   render() {
-    return (
-      <div className="SearchBar">
-  <div className="SearchBar-sort-options">
-    <ul>
-      SearchBar.renderSortByOptions();
-    </ul>
-  </div>
-  <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
-  </div>
-  <div className="SearchBar-submit">
-    <a>Lets Go</a>
-  </div>
-</div>
-    )
-  }
-}
+   return (
+     <div className="SearchBar">
+       <div className="SearchBar-sort-options">
+         <ul>
+           {this.renderSortByOptions()}
+         </ul>
+       </div>
+       <div className="SearchBar-fields">
+         <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+         <input placeholder="Where?" onChange={this.handleLocationChange} />
+       </div>
+       <div className="SearchBar-submit">
+         <a>Let&#39;s Go</a>
+       </div>
+     </div>
+   )
+ }
 
 export default SearchBar;

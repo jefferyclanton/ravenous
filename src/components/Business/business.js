8bc06f24d1216d4a774ac24d2bd0476d0c1f0ff4
jefferyclanton <37197975+jefferyclanton@users.noreply.github.com>
2018-04-17 10:44:56 -0400

    Organize file structure

--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './business.css';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
- name: 'MarginOtto Pizzeria',
- address: '1010 Paddington Way',
- city: 'Flavortown',
- state: 'NY',
- zipCode: '10101',
- category: 'Italian',
- rating: 4.5,
- reviewCount: 90
-}
 
 class Business extends React.Component {
   render() {

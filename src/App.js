48eb0aab97506a7183f48db34e6cee89baf5d4ba
jefferyclanton <37197975+jefferyclanton@users.noreply.github.com>
2018-04-18 10:07:35 -0400

    Create more concise components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import './App.css';
 import BusinessList from './components/BusinessList/businesslist.js';
 import SearchBar from "./components/SearchBar/searchbar.js";
 
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+  <div className="image-container">
+    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+  </div>
+  <h2>{this.name}</h2>
+  <div className="Business-information">
+    <div className="Business-address">
+      <p>{this.address}</p>
+      <p>{this.city}</p>
+      <p>{this.state} {this.zipCode}</p>
+    </div>
+    <div className="Business-reviews">
+      <h3>{this.category}</h3>
+      <h3 className="rating">{this.rating} stars}</h3>
+      <p>{this.reviews} reviews</p>
+    </div>
+  </div>
+</div>
+    )
+  }
+}
+
+const businesses = [<Business />, <Business />, <Business />, <Business />, <Business />, <Business />];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
   <h1>ravenous</h1>
     <SearchBar />
-    <BusinessList />
+    <BusinessList propName={businesses}/>
 </div>
     );
   }
--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -9,17 +9,17 @@ class Business extends React.Component {
   <div className="image-container">
     <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
   </div>
-  <h2>{this.name}</h2>
+  <h2>{this.props.name}</h2>
   <div className="Business-information">
     <div className="Business-address">
-      <p>{this.address}</p>
-      <p>{this.city}</p>
-      <p>{this.state} {this.zipCode}</p>
+      <p>{this.props.address}</p>
+      <p>{this.props.city}</p>
+      <p>{this.props.state} {this.props.zipCode}</p>
     </div>
     <div className="Business-reviews">
-      <h3>{this.category}</h3>
-      <h3 className="rating">{this.rating} stars}</h3>
-      <p>{this.reviews} reviews</p>
+      <h3>{this.props.category}</h3>
+      <h3 className="rating">{this.props.rating} stars}</h3>
+      <p>{this.props.reviews} reviews</p>
     </div>
   </div>
 </div>
